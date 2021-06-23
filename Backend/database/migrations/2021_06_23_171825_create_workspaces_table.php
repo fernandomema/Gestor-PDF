2ae7f5012a3fe96d2831fec78ca1d68beb484735
Fernando Merino <fernandomema@gmail.com>
2021-06-23 19:28:29 +0200

    Create 2021_06_23_171825_create_workspaces_table.php

---
+++ Backend/database/migrations/2021_06_23_171825_create_workspaces_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserWorkspaceTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('user_workspace', function(Blueprint $table) {
+			$table->integer('user_id')->unsigned()->index();
+      $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+      
+      $table->integer('workspace_id')->unsigned()->index();
+      $table->foreign('workspace_id')->references('id')->on('workspaces')->onDelete('cascade');
+
+      $table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('user_workspace');
+	}
+}
