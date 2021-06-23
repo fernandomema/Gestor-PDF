b4de615041e148da47434acb49f0bdb15139a420
Fernando Merino <fernandomema@gmail.com>
2021-06-23 19:23:28 +0200

    Create 2021_06_23_171819_create_documents_table.php

---
+++ Backend/database/migrations/2021_06_23_171819_create_documents_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDocumentsTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('documents', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 250);
+			$table->string('type', 50);
+			$table->longText('document');
+			$table->json('data');
+      $table->unsignedBigInteger('workspace_id');
+			$table->timestamps();
+      
+      $table->foreign('workspace_id')->references('id')->on('workspaces');
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('documents');
+	}
+}
