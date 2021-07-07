71ccf89354be8d1f030bb2722abb6813a8999418
fernandomema <fernandomema@gmail.com>
2021-07-07 15:22:33 +0200

    Add dark theme support

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
+
+    // check user color theme
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        // set dark theme
+        $('body').addClass('theme-dark');
+    }
+    
+    // Load avatar
     $('.avatar.avatar-sm').css('background-image', 'url(' + sessionStorage.getItem('avatar') + ')');
+
 });
