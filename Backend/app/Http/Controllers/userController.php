734987f01e551c72b759d3c15bbbf051bdaf39ea
Ayrton Eli Hernández Casas <76624014+JesusHernandez1995@users.noreply.github.com>
2021-07-15 16:01:52 +0200

    workspace model added

--- Backend/app/Http/Controllers/userController.php
+++ Backend/app/Http/Controllers/userController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Workspace;
 use Illuminate\Support\Str;
 use Auth;
 use Illuminate\Support\Facades\Password;
