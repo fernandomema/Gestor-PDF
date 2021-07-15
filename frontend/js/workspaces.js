aab791ca01e0f2d31249d6e34494a2db2f14988d
fernandomema <fernandomema@gmail.com>
2021-07-15 16:13:31 +0200

    Load workspaces from api

---
+++ frontend/js/workspaces.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    // AJAX para conectarnos con la API
+    $.ajax({
+        type: "GET",
+        url: "https://insta-pdf.herokuapp.com/api/workspaces",
+        headers: {
+            'Accept': 'application/json',
+            'Authorization':'Bearer '+sessionStorage.getItem('token')
+        },
+        dataType: 'JSON',
+        success: function (workspaces) {
+            console.log(workspaces);
+            workspaces.forEach(function (workspace) {
+                workspace.avatar = "https://eu.ui-avatars.com/api/?name="+workspace.name+"?background=random";
+                //$("#"+workspace.name).loadTemplate($("#document-template"), workspace.documents);
+            });
+            $("#workspace").loadTemplate($("#workspace-template"), workspaces);
+            // documents.forEach(function (document) {
+            //     $("#document-container").loadTemplate($("#document-template"), {
+            //         title: document.name,
+            //     });
+            // })            
+        },
+        error: function(response){
+            console.log(response);
+            if (response.message == 'Unauthenticated' || response.status == 401) {
+                window.location.href = "login.html";
+            }
+        }
+    });
+});
