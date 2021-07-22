6bd8dd37aad63f9302b5aae9b4fbdab09577c6fc
Ayrton Eli Hernández Casas <76624014+JesusHernandez1995@users.noreply.github.com>
2021-07-22 15:34:02 +0200

    updated seearch documents in workspaces javascript

--- frontend/js/api_search_document_in_workspaces.js
+++ frontend/js/api_search_document_in_workspaces.js
@@ -31,17 +31,37 @@ $(document).ready(function () {
                 data: datos,
                 dataType: 'JSON',
                 success: function (workspaces) {
-                    console.log(workspaces);
-
-                    workspaces.forEach(function (workspace){
-                        if(workspace.documents.length > 0){
+                    // traemos de manera dinámica los workspaces y los insertamos en el DOM
+                    workspaces.forEach(function (workspace) {
+                        workspace.elemID = workspace.name.replace(/\s+/g, '-');
+                        workspace.upload_url = 'upload.html?workspace_id='+workspace.id;
+                    });
+        
+                    // Cargamos la plantilla workgroup template
+                    $("#workgroups").loadTemplate($("#workgroup-template"), workspaces);
+        
+                    
+                    // Dentro de cada workspace, subimos cada archivo en ese workspace
+                    workspaces.forEach(function (workspace) {
+                        if (workspace.documents.length > 0) {
                             workspace.documents.forEach(function (doc) {
                                 doc.preview = 'preview.html?id=' + doc.id;
                                 doc.sign = 'sign.html?id=' + doc.id;
                             });
                             $("#"+workspace.name.replace(/\s+/g, '-')).loadTemplate($("#document-template"), workspace.documents);
+                        } else {
+                            $("#"+workspace.name.replace(/\s+/g, '-')).loadTemplate($("#empty-template"), workspace);
                         }
                     });
+        
+                    // Borramos el botón upload documents a aquellos workspaces donde no haya ningún documento
+                    $('.empty').parent().parent().parent().remove();
+        
+                    // Si el número de workspaces es impar, el último div tendrá la clase col-12.
+                    if (workspaces.length % 2 == 1) {
+                        $("#workgroups > div").last().removeClass("col-lg-6");
+                    }
+                    registerDeleteButtons();
                 },
                 error: function(response){
                     console.log(response);
