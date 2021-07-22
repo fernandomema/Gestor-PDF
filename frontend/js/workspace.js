5fe1c5be49164bce499d4732b3417c5afc2cb6bd
fernandomema <fernandomema@gmail.com>
2021-07-22 15:17:19 +0000

    Show only requested workspace

--- frontend/js/workspace.js
+++ frontend/js/workspace.js
@@ -2,24 +2,21 @@ $(document).ready(function () {
     // AJAX para conectarnos con la API
     $.ajax({
         type: "GET",
-        url: "https://insta-pdf.herokuapp.com/api/workspaces",
+        url: "https://insta-pdf.herokuapp.com/api/workspaces/" + findGetParameter('id'),
         headers: {
             'Accept': 'application/json',
             'Authorization':'Bearer '+sessionStorage.getItem('token')
         },
         dataType: 'JSON',
-        success: function (workspaces) {
-            workspaces.forEach(function (workspace) {
-                workspace.elemID = workspace.name.replace(/\s+/g, '-');
-            });
-            $("#workgroups").loadTemplate($("#workgroup-template"), workspaces);
-            workspaces.forEach(function (workspace) {
-                if (workspace.documents.length > 0) {
-                    $("#"+workspace.name.replace(/\s+/g, '-')).loadTemplate($("#document-template"), workspace.documents);
-                } else {
-                    $("#"+workspace.name.replace(/\s+/g, '-')).loadTemplate($("#empty-template"));
-                }
-            });           
+        success: function (data) {
+            console.log(data);
+            data.workspace.elemID = data.workspace.name.replace(/\s+/g, '-');
+            $("#workgroups").loadTemplate($("#workgroup-template"), data.workspace);
+            if (data.workspace.documents.length > 0) {
+                $("#"+data.workspace.name.replace(/\s+/g, '-')).loadTemplate($("#document-template"), data.workspace.documents);
+            } else {
+                $("#"+data.workspace.name.replace(/\s+/g, '-')).loadTemplate($("#empty-template"));
+            }          
         },
         error: function(response){
             console.log(response);
