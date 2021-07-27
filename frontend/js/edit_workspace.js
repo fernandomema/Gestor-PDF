349884beb1abcac42bd9a69f72455f78a4460022
fernandomema <fernandomema@gmail.com>
2021-07-27 15:17:56 +0000

    Add button and modal for add user

--- frontend/js/edit_workspace.js
+++ frontend/js/edit_workspace.js
@@ -26,4 +26,35 @@ $(document).ready(function () {
             }
         }
     });
+
+    $('#adduser-button').on('click', function() {
+        $.ajax({
+            type: "GET",
+            url: "https://insta-pdf.herokuapp.com/api/workspaces/" + findGetParameter('id') + '/join/' + $('input[name=user]').val,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization':'Bearer '+sessionStorage.getItem('token')
+            },
+            dataType: 'JSON',
+            success: function (data) {
+                console.log(data);
+
+            },
+            error: function(response){
+                console.log(response);
+                if (response.message == 'Unauthenticated' || response.status == 401) {
+                    window.location.href = "login.html";
+                }
+            }
+        });
+    })
+
+    $('#modal-alert').iziModal({
+        headerColor: '#7cd438', 
+        width: 400,
+        timeout: 10000, 
+        pauseOnHover: true,
+        timeoutProgressbar: true,
+        attached: 'bottom' 
+    });
 });
